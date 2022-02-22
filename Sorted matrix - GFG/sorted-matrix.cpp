55ae79cadcae7850567891fd076558c16fbfd6ec
Girish Kumar <85754888+girishchhonkar19@users.noreply.github.com>
2022-02-22 15:29:53 +0530

    Added solution - LeetHub

---
+++ Sorted matrix - GFG/sorted-matrix.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> sortedMatrix(int N, vector<vector<int>> Mat) {
+        vector<vector<int>>ans(N,vector<int>(N));
+        
+        vector<int>v;
+        for(int i=0;i<Mat.size();i++)
+        {
+            for(int j=0;j<Mat[0].size();j++)
+              v.push_back(Mat[i][j]);
+        }
+        
+        sort(v.begin(),v.end());
+       int k=0;
+       for(int i=0;i<N;i++)
+       {
+           for(int j=0;j<N;j++)
+           {
+              ans[i][j]=v[k++]; 
+           }
+       }
+       return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> v(N, vector<int>(N));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) cin >> v[i][j];
+        Solution ob;
+        v = ob.sortedMatrix(N, v);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) cout << v[i][j] << " ";
+            cout << "\n";
+        }
+    }
+}  // } Driver Code Ends
